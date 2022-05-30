03ec494c6c832d91ff52d0e181e2eae742570565
Ruslan Kosenkov <r.kosenkov@spb.mcart.ru>
2022-05-30 23:39:21 +0300

    Notes app

---
+++ notes/src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <h1>{{ title }}</h1>
+
+          <message v-if="message" :message="message" />
+
+          <!-- TODO New note -->
+          <div class="new-note">
+            <input v-model="note.title" type="text" />
+            <textarea v-model="note.descr"></textarea>
+            <button @click="addNote">New Note</button>
+          </div>
+
+          <!-- TODO Note list -->
+          <div class="notes">
+            <div class="note" v-for="(note, index) in notes" :key="index">
+              <div class="note-header">
+                <p>{{ note.title }}</p>
+              </div>
+              <div class="note-body">
+                <p>{{ note.descr }}</p>
+                <span> {{ note.date }} </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import message from '@/components/Message.vue'
+export default {
+  name: 'App',
+  components: {
+    message
+  },
+  data() {
+    return {
+      title: 'Notes App',
+      message: null,
+      note: {
+        title: '',
+        descr: '',
+      },
+      notes: [
+        {
+          title: 'First Note',
+          descr: 'Description for first Note',
+          date: new Date(Date.now()).toLocaleString(),
+        },
+        {
+          title: 'Second Note',
+          descr: 'Description for second Note',
+          date: new Date(Date.now()).toLocaleString(),
+        },
+        {
+          title: 'Third Note',
+          descr: 'Description for third Note',
+          date: new Date(Date.now()).toLocaleString(),
+        },
+      ],
+    }
+  },
+
+  methods: {
+    //TODO Adding new note with validation
+    addNote() {
+      const { title, descr } = this.note
+
+      if (title === '') {
+        this.message = 'Заголовок не может быть пустым'
+        return false
+      }
+
+      this.notes.push({
+        title,
+        descr,
+        date: new Date(Date.now()).toLocaleString(),
+      })
+
+      this.message = null
+      this.note.title = ''
+      this.note.descr = ''
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ notes/src/components/Message.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="message">
+  <p>Message Component - {{ message }}</p>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true,
+      default: 'default message'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .message {
+    text-align: center;
+    padding: 20px;
+  }
+</style>
